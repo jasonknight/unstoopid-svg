aeacefaa8a1d9e31823bc796d4e6ce5b8e38360c
Jason Knight <jason@jason-knight-martin.com>
2013-06-13 15:08:46 -0400

    Created Simple Plugin

--- unstoopid-svg.php
+++ unstoopid-svg.php
@@ -12,4 +12,7 @@ function unstoopid_svg_allow_svg( $mimes ) {
 	$mimes['svg'] = "image/svg+xml";
 	return $mimes;
 }
-add_filter( 'upload_mimes', 'unstoopid_svg_allow_svg');
+function unstoopid_svg_init() {
+  add_filter( 'upload_mimes', 'unstoopid_svg_allow_svg');
+}
+add_action( 'init', 'unstoopid_svg_init' );
